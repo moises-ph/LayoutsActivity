f8626fb8232945e672c91dc09d3a1ed41bc4e6b8
moisesph <mois.mp8@gmail.com>
2023-02-27 15:14:31 -0500

    `Table and Frame layout`

--- app/src/main/java/com/example/layoutsactivity/TableLayout.kt
+++ app/src/main/java/com/example/layoutsactivity/TableLayout.kt
@@ -4,15 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class TableLayout : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_table_layout)
     }
-    fun abrirTable(view : View){
-        val intent = Intent(this, TableLayout::class.java).apply{ }
+    fun volverInicio(view : View){
+        val intent = Intent(this, MainActivity::class.java).apply{ }
         startActivity(intent)
     }
 }
