8ef69a423387ebf276f070393fd207c6d34213ff
moisesph <mois.mp8@gmail.com>
2023-02-27 22:19:48 -0500

    Terminado

--- app/src/main/java/com/example/layoutsactivity/LinealHorizontal.kt
+++ app/src/main/java/com/example/layoutsactivity/LinealHorizontal.kt
@@ -1,11 +1,18 @@
 package com.example.layoutsactivity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class LinealHorizontal : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lineal_horizontal)
+    }
+
+    fun InicioH(view : View){
+        val intent = Intent(this, MainActivity::class.java).apply {  }
+        startActivity(intent)
     }
 }
